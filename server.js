6b833b3acada2248ddaba953e82a60343c5d0140
LuisAntonioCardoso <luis.antonio.c.of@gmail.com>
2023-05-19 17:27:57 +0100

    added valid cities and updated endpoint to return data from 1 city

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 // ! dependencies
 import express from 'express';
+import {fetchWeatherData} from './lib/utils.js';
 
 const app = express();
 
+const validCities = new Map([
+	['Lisboa', 2267056],
+	['Leiria', 2267094],
+	['Coimbra', 2740636],
+	['Porto', 2735941],
+	['Faro', 2268337]
+]);
+
 // ! endpoint
-app.get('/', async (request, response) => {
-	response.send('hello world');
+app.get('/weather/', async (request, response) => {
+	const city = 'Lisboa';
+
+	// ! OpenWeatherMap API request
+	try {
+		const weatherData = await fetchWeatherData(validCities.get(city));
+
+		console.log(`Informação sobre ${city} obtida da API OpenWeatherMap.`);
+		return response.json(weatherData);
+	} catch (error) {
+		console.error(`Não foi possivel obter informação sobre o clima para a ${city}.`, error);
+		return response.status(500).json({error: 'Erro ao obter informação.'});
+	}
 });
 
 app.listen(3000, () => {
