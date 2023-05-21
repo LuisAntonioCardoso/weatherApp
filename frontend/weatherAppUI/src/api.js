efb48889bd455c00b8fc7511b1d4dc8735113b2a
LuisAntonioCardoso <luis.antonio.c.of@gmail.com>
2023-05-21 13:54:44 +0100

    add setup for GeoDB API

---
+++ frontend/weatherAppUI/src/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL =
+	'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=Q45&minPopulation=100000';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '82859e8563mshc9cc46c87c58179p1df695jsn16373146786d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
